c382116128f773dc103f80f60babc1f38194a86b
amirhmi <amirhmi1377@gmail.com>
2023-02-04 22:19:41 -0800

    Added pre-populate-db script

---
+++ pre-populate-db.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+
+from memberManagement.models import Team, UserProfile, Membership
+
+members = [
+    {"first_name": "Rick",
+     "last_name": "Noval",
+     "email": "noval@gmail.com",
+     "phone_number": "+16048298769",
+     "role": "AD"},
+
+    {"first_name": "Susan",
+     "last_name": "Conor",
+     "email": "Susan@outlook.com",
+     "phone_number": "+17288298567",
+     "role": "RE"},
+
+    {"first_name": "Margo",
+     "last_name": "Barr",
+     "email": "Barr@yahoo.com",
+     "phone_number": "+16045298765",
+     "role": "RE"},
+
+    {"first_name": "Ronald",
+     "last_name": "Lum",
+     "email": "Lum@ubc.com",
+     "phone_number": "+16048238764",
+     "role": "AD"},
+
+    {"first_name": "Melvin",
+     "last_name": "Forbis",
+     "email": "Forbis@gmail.com",
+     "phone_number": "+16048298763",
+     "role": "AD"},
+]
+
+team = Team.objects.create(name="course-team")
+team.save()
+for member in members:
+    user = User.objects.create(username=member["email"], email=member["email"], first_name=member["first_name"],
+                               last_name=member["last_name"])
+    user_profile = UserProfile.objects.create(user=user, phone_number=member["phone_number"])
+    membership = Membership.objects.create(team=team, user_profile=user_profile)
+
+    user.save()
+    user_profile.save()
+    membership.save()
